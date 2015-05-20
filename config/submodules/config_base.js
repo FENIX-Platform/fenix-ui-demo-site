f6cca954ba44fa3e1592a3637837a1d19405fd6c
DanieleO-fenix <daniele.olivotti@fao.org>
2015-05-20 14:19:23 +0200

    Configuration change

--- config/submodules/config_base.js
+++ config/submodules/config_base.js
@@ -6,8 +6,12 @@ define([], function () {
     //Use the following example to override properties:
     //services.SERVICE_BASE_ADDRESS = "http://fenix.fao.org/d3s_dev2/msd";
     var services = {
+        //Dev
         SERVICE_BASE_ADDRESS: "http://fenix.fao.org/d3s_dev/msd"
+        //Demo
         //SERVICE_BASE_ADDRESS: 'http://fenix.fao.org/d3s_fenix/msd'
+        //prod
+        //SERVICE_BASE_ADDRESS: "http://fenixservices.fao.org/d3s/msd"
     };
 
     return services;
